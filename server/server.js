ea55d8cecf3260fd328accc7a150b51ef38e87a6
ZHUANG <Rui.Zhuang@woodplc.com>
2022-08-03 12:53:51 -0400

    server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,15 @@
 const express = require('express');
 const app = express();
 
+const bodyParser = require('body-parser');
+const { urlencoded } = require('express');
+
+app.use(bodyParser, urlencoded({ extended: true }))
+
+
+
+
+
 app.listen(3000, () => {
     console.log("server is running on 3000")
 })
