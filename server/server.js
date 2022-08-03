25e11318a138cf3650c9583306b62d86fa9a3095
ZHUANG <Rui.Zhuang@woodplc.com>
2022-08-03 13:58:22 -0400

    mongo initiate

--- server/server.js
+++ server/server.js
@@ -1,15 +1,28 @@
 
 const express = require('express');
 const app = express();
-
+const mongoose = require('mongoose')
 const bodyParser = require('body-parser');
 const { urlencoded } = require('express');
 
+
+
 app.use(bodyParser, urlencoded({ extended: true }))
 
+//connect to mongodb and look for ip_data_react table, if does not exist, it will create one
+mongoose.connect("mongodb://localhost:27017/ip_data_react", { useNewUrlParser: true })
 
+const ipSchema = new mongoose.Schema({
+    address: String
+})
 
+const ipaddress = mongoose.model('ipaddress', ipSchema)
 
+const ips = new ipaddress({
+    address: 'test'
+})
+
+ips.save();
 
 app.listen(3000, () => {
     console.log("server is running on 3000")
