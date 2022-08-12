d1725905c9c7d1cdc7bc5f445a2210ee391788a4
ZHUANG <Rui.Zhuang@woodplc.com>
2022-08-12 09:48:06 -0400

    update img

--- nwapp/src/components/NavBar.js
+++ nwapp/src/components/NavBar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from "react-router-dom"
 import './NavBar.css'
-import image from '../img/nwlogo.png'
+import image from '../img/nwlogo1.jpg'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -9,12 +9,12 @@ import Navbar from 'react-bootstrap/Navbar';
 export default function NavBar() {
     return (
 
-        <Navbar bg="transparent" variant="black" >
+        <Navbar bg="rgb(81, 70, 138)" variant="dark" className='navbar'>
             <Container className='header'>
                 <Navbar.Brand href="#home" >
                     <img
                         src={image}
-                        width="30"
+                        width="170"
                         height="30"
                         className="d-inline-block align-top"
                         alt=''
