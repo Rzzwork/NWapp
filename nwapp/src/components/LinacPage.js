3176566fb5063c0bf858b4632d84308b0f7854f2
ZHUANG <Rui.Zhuang@woodplc.com>
2022-08-12 10:22:25 -0400

    linac and mq page test

--- nwapp/src/components/LinacPage.js
+++ nwapp/src/components/LinacPage.js
@@ -1,12 +1,60 @@
 import React from 'react'
 import './LinacPage.css'
+import Table from 'react-bootstrap/Table';
 
 export default function LinacPage() {
     return (
-        <div className='container'>
-            <h5>Monthly QA</h5>
-            <h5>Annual QA</h5>
-            <h5>Patient QA</h5>
+        <div className='homepage container'>
+            <Table striped>
+                <thead>
+                    <tr>
+                        <th>Action</th>
+                        <th>Test/Cycle</th>
+                        <th>Due Date</th>
+                        <th>Unit</th>
+                        <th>Frenquency</th>
+                        <th>Assigned To</th>
+                        <th>Completed</th>
+                        <th>Pass/Fail</th>
+                        <th>Review Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Monthly QA</td>
+                        <td></td>
+                        <td></td>
+                        <th></th>
+                        <td></td>
+                        <td></td>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Annual QA</td>
+                        <td></td>
+                        <td></td>
+                        <th></th>
+                        <td></td>
+                        <td></td>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>Patient QA</td>
+                        <td></td>
+                        <td></td>
+                        <th></th>
+                        <td></td>
+                        <td></td>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                </tbody>
+            </Table>
         </div>
     )
 }
--- nwapp/src/components/MQPage.js
+++ nwapp/src/components/MQPage.js
@@ -1,13 +1,71 @@
 import React from 'react'
 import './MQPage.css'
+import Table from 'react-bootstrap/Table';
 
 export default function MQPage() {
     return (
-        <div className='container'>
-            <h5>New Chart Check</h5>
-            <h5>Weekly Chart Check</h5>
-            <h5>Auto Form Generation</h5>
-            <h5>Workflow Efficiency Evaluation</h5>
+        <div className='homepage container'>
+            <Table striped>
+                <thead>
+                    <tr>
+                        <th>Action</th>
+                        <th>Test/Cycle</th>
+                        <th>Due Date</th>
+                        <th>Unit</th>
+                        <th>Frenquency</th>
+                        <th>Assigned To</th>
+                        <th>Completed</th>
+                        <th>Pass/Fail</th>
+                        <th>Review Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>New Chart Check</td>
+                        <td></td>
+                        <td></td>
+                        <th></th>
+                        <td></td>
+                        <td></td>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Weekly Chart Check</td>
+                        <td></td>
+                        <td></td>
+                        <th></th>
+                        <td></td>
+                        <td></td>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>Auto Form Generation</td>
+                        <td></td>
+                        <td></td>
+                        <th></th>
+                        <td></td>
+                        <td></td>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                    <tr>
+                        <td>4</td>
+                        <td>Workflow Efficiency Evaluation</td>
+                        <td></td>
+                        <td></td>
+                        <th></th>
+                        <td></td>
+                        <td></td>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                </tbody>
+            </Table>
         </div>
     )
 }
